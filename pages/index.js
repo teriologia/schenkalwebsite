de46f5f8e3d7b328e06e64e8273946b9891839ef
Serdar Tekin <mserdar.tekin@gvg.com.tr>
2021-03-23 18:11:33 +0300

    new

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
-import react from 'react'
+import react, { useState, useEffect } from 'react'
 import styles from '../styles/Home.module.css'
 import schenkal from '../public/schenkal.jpg'
 
 export default function Home() {
-  
+  const [isyes, setIsYes] = useState(0)
+  useEffect(() => {
+    console.log(isyes)
+  })
   return (
-    <div className={styles.container} style={{justifyContent: 'center', alignItems: 'center'}}>
-     <p className={styles.sad} style={{ fontSize: 72, color :'white'}}>BAŞINI ALDIK ALLAH BELAMIZI VERDİ</p>
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <span>Bu kişi size götten giriş izni istiyor. İzin vermeniz dahilinde bu kişi size götten giriş hakkına sahip olucak</span>
+        <span>bknz: götten giriş yasası 25. madde D. fıkrası</span>
+        <div style={{display:'flex', justifyContent: 'space-around', width: '100%', height: '100%', alignItems: 'flex-end'}}>
+          <div onClick={() => setIsYes(1)} className={styles.button} style={{backgroundColor: '#00FF00'}}>Kabul Et</div>
+          <div onClick={() => setIsYes(2)} className={styles.button}>Reddet</div>
+        </div>
+      </div>
     </div>
   )
 }
